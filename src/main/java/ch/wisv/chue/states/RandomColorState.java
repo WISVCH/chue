efbaf47739ffdb4b7ee7cb2d4ef7848c3cf3d088
Rick Wieman <git@rickw.nl>
2015-09-25 13:01:39 +0200

    Random returns the color of the affected lamp(s)
    Added Map with random colors to the RandomColorState.
    Colors are set using a random hue value, and full saturation/brightness.
    Fixes #17

--- src/main/java/ch/wisv/chue/states/RandomColorState.java
+++ src/main/java/ch/wisv/chue/states/RandomColorState.java
@@ -3,13 +3,23 @@ package ch.wisv.chue.states;
 import ch.wisv.chue.HueService;
 import com.philips.lighting.model.PHBridge;
 import com.philips.lighting.model.PHLightState;
+import javafx.scene.paint.Color;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
  * Random color loop state
  */
 public class RandomColorState implements HueState {
+
+    private Map<String, Color> lightColors = new HashMap<>();
+
+    public Map<String, Color> getLightColors() {
+        return lightColors;
+    }
+
     @Override
     public void execute(PHBridge bridge, String... lightIdentifiers) {
         Random rand = new Random();
@@ -19,6 +29,11 @@ public class RandomColorState implements HueState {
 
             PHLightState lightState = new PHLightState();
             lightState.setHue(randHue);
+            lightState.setSaturation(HueService.MAX_SATURATION);
+            lightState.setBrightness(HueService.MAX_BRIGHTNESS);
+
+            lightColors.put(id, Color.hsb((double) (randHue * 360) / HueService.MAX_SATURATION, 1, 1));
+
             bridge.updateLightState(id, lightState, null); // If no bridge response is required then use this simpler form.
         }
     }
